da3435f7499eef84b379d532c6dadf196ec954a2
Tom Deseyn <tom.deseyn@gmail.com>
2018-05-29 05:30:09 -0400

    Copy 2.0 as base for 2.1

---
+++ 2.1/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+#
+# Test the Jenkins image.
+#
+# IMAGE_NAME specifies the name of the candidate image used for testing.
+# The image has to be available before this script is executed.
+#
+
+IMAGE_NAME=${IMAGE_NAME:-dotnet/dotnet-20-jenkins-slave-rhel7}
+
+if [ "${DEBUG}" != "" ]; then
+  # print commands as they are executed.
+  set -x
+fi
+
+info() {
+  echo -e "\e[1m[INFO] $@\e[0m"
+}
+
+info "Testing ${IMAGE_NAME}"
+
+# make sure oc is installed
+docker run ${IMAGE_NAME} oc
+# make sure dotnet, npm and nodejs are available
+# the default entrypoint for the image assumes if you supply more than
+# one arg, you are trying to invoke the slave logic, so we have to
+# override the entrypoing to run complicated commands for testing.
+docker run --entrypoint="/bin/bash" ${IMAGE_NAME} "-c" "dotnet --version"
+docker run --entrypoint="/bin/bash" ${IMAGE_NAME} "-c" "npm --version"
+docker run --entrypoint="/bin/bash" ${IMAGE_NAME} "-c" "node --version"
+
+info "Testing ${IMAGE_NAME} finished succesfully"
